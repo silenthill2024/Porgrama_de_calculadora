8f3df914319f5264ddf81142500377c3ab4cc193
SL2705SL2705 <al23311643@utcj.edu.mx>
2024-11-25 19:17:31 -0700

    eliminacion de pantallas y agregar la funcion de la calculadora basica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.porgramadecalculadora"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.porgramadecalculadora"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("net.objecthunter:exp4j:0.4.8")
+
 }
